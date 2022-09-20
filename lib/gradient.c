8b66e1692821781c53fb4d244ae210298e4eb04b
eskylo <eskylo@hotmail.com>
2022-09-20 20:55:52 -0300

    apply gradient proportionally to the width

--- lib/gradient.c
+++ lib/gradient.c
@@ -6,7 +6,7 @@ void gradient(unsigned char *buf, int width, int height) {
 
     for (i=0; i<width; i++) {
         for (j=0; j<height; j++) {
-            k = buf[j*height+i]-i;
+            k = buf[j*height+i]-i*255/width;
             if (k<0) k=0;
             buf[j*height+i] = k;
         }
